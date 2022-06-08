f2a9b5ef7e563f9f197953d5d74db6b317042427
Marc Zeugin <marc_zeugin@yahoo.com>
2022-06-08 15:43:10 +0200

    added more models

--- models.py
+++ models.py
@@ -1,17 +1,18 @@
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
-from sklearn.linear_model import LinearRegression, RidgeCV, LassoCV, LassoLarsCV, BayesianRidge
+from sklearn.linear_model import LinearRegression, LogisticRegressionCV, RidgeCV, LassoCV, LassoLarsCV, BayesianRidge, PoissonRegressor, PassiveAggressiveRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor, StackingRegressor
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.neural_network import MLPRegressor
 from sklearn.tree import ExtraTreeRegressor
-from sklearn.svm import SVR, LinearSVC
+from sklearn.svm import SVR, LinearSVR
 
 
 def predict_regression(model_types, X_train, y_train, X_test):
     
     models = {'LR': LinearRegression(), 'RF': RandomForestRegressor(), 'SVM': SVR(), 'GBR': GradientBoostingRegressor(), 'ABR': AdaBoostRegressor(),
               'GPR': GaussianProcessRegressor(), 'RCV': RidgeCV(),'LLCV': LassoLarsCV(), 'BR': BayesianRidge(), 'KNN': KNeighborsRegressor(), 
-              'LCV': LassoCV(), 'MLP': MLPRegressor(), 'LSVM': LinearSVC(), 'ET': ExtraTreeRegressor()}
+              'LCV': LassoCV(), 'MLP': MLPRegressor(), 'LSVM': LinearSVR(), 'ET': ExtraTreeRegressor(), 'SR': StackingRegressor(),
+              'LRCV': LogisticRegressionCV(), 'PR': PoissonRegressor(), 'PAR': PassiveAggressiveRegressor()}
     y_pred = {}
     
     for model_type in model_types:

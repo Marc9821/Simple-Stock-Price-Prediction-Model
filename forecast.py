b5662364a0edce336347f9aad3387eaeebc9d5a7
Marc9821 <marc_zeugin@yahoo.com>
2022-06-21 20:07:21 +0200

    added initial version of forecast with finnhub API

--- forecast.py
+++ forecast.py
@@ -1,14 +1,25 @@
 from get_stock_data import get_stock_data
+from dotenv import load_dotenv
+load_dotenv()
+import finnhub
+import os
 
+
+FINNHUB_API_KEY = os.getenv('FINNHUB_API_KEY') # you need to register your own API key at https://finnhub.io/
+finnhub_client = finnhub.Client(api_key=FINNHUB_API_KEY)
 
 def user_input():
     stock_name = []
     stock_name.append(input("Please enter a Stock Ticker to predict: ").upper())
     
-    get_data = get_stock_data(stock_name)
-    
-    if get_data is False:
+    # retrieve peers of selected company
+    try:
+        peer_list = finnhub_client.company_peers(stock_name)
+    except:
+        print('Ticker not found!')
         user_input()
+    
+    new_stock_data = get_stock_data(peer_list)
                 
     return stock_name
 
